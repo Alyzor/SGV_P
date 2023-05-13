aa0b58ae9e632b208e907fe5a3207636a0d7a1c1
Alyzor <pereirapedro591@gmail.com>
2023-05-13 20:18:30 +0100

    Add project files.

---
+++ Classes/CRUD.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SGV_P.Classes
+{
+    internal class CRUD
+    {
+        public class Clientes
+        {
+            public int id { get; set; }
+            public string nome { get; set; }
+            public string DataNasc { get; set; }
+            public string Email { get; set; }
+            public string Telm { get; set; }
+            public string NIF { get; set; }
+            public string Morada { get; set; }
+        }
+        public class Fornecedores
+        {
+            public int id { get; set; }
+            public string nome { get; set; }
+            public string NIF { get; set; }
+            public string Morada { get; set; }
+        }
+        public class Vendas
+        {
+            public int id { get; set; }
+            public int idCliente { get; set; }
+            public DateTime Data { get; set; }
+            public decimal PrecoTotal { get; set; }
+        }
+        public class VendasDetalhes
+        {
+            public int id { get; set; }
+            public int idVenda { get; set; }
+            public int idProduto { get; set; }
+            public int Quantidade { get; set; }
+            
+        }
+        public class Encomendas
+        {
+            public int id { get; set; }
+            public int idFornecedor { get; set; }
+            public DateTime DataEncomenda { get; set; }
+            public DateTime DataChegada { get; set; }
+            public decimal PrecoTotal { get; set; }
+        }
+        public class EncomendasDetalhes
+        {
+            public int id { get; set; }
+            public int idEncomenda { get; set; }
+            public int idProduto { get; set; }
+            public int Quantidade { get; set; }
+            
+        }
+        public class Produtos
+        {
+            public int id { get; set; }
+            public string Designacao { get; set; }
+            public string Descricao { get; set; }
+            public int Stock { get; set; }
+            public decimal Preco { get; set; }
+            public int idFornecedor { get; set; }
+        }
+        public static string GetConnection()
+        {
+            try
+            {
+                
+            
+            string connectionString = "datasource=localhost;port=3306;username=root";
+            MySqlConnection con = new MySqlConnection(connectionString);
+                string query = "Use Database sgvdb;";
+                query += "Select * from ";
+                con.Open();
+            }
+            catch (Exception ex)
+            {
+                return "no " + ex;
+            }
+
+        }
+    }
+}
